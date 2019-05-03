b20a757cc70784c94d6d4538d451e3d18025f101
SanderMiller <amiller@olin.edu>
2019-05-03 12:43:53 -0400

    Perfected Roatation

--- boat1.py
+++ boat1.py
@@ -10,7 +10,7 @@ YELLOW = (255, 255, 63)
 class Boat(pygame.sprite.Sprite):
     #This class represents a car. It derives from the "Sprite" class in Pygame.
 
-    def __init__(self, color, width, height):
+    def __init__(self, pos):
         # Call the parent class (Sprite) constructor
         super().__init__()
 
@@ -26,9 +26,9 @@ class Boat(pygame.sprite.Sprite):
         self.image = boat_image
         self.rotated_image = self.image
 
-        self.rect = self.image.get_rect(center=pos)
+        self.rect = self.image.get_rect(center=(pos))
         self.pos = Vector2(pos)
-        self.offset = Vector2(120, 0)
+        self.offset = Vector2(0, 0)
         self.angle = 0
 
 
@@ -47,12 +47,16 @@ class Boat(pygame.sprite.Sprite):
 
     def moveRight(self, pixels):
         self.rect.x += pixels
+        self.pos.x += pixels
 
     def moveLeft(self, pixels):
         self.rect.x -= pixels
+        self.pos.x -= pixels
 
     def moveUp(self, pixels):
         self.rect.y -= pixels
+        self.pos.y -= pixels
 
     def moveDown(self, pixels):
         self.rect.y += pixels
+        self.pos.y += pixels
