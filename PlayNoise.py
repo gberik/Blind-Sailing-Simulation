ce94c654eaeacce2b62de975fdf63a3eaa064a7f
Sina <47065325+gberik@users.noreply.github.com>
2019-05-07 03:15:48 -0400

    Remove old files

--- PlayNoise.py
+++
@@ -1,7 +0,0 @@
-
-import os
-# must pip install sox
-# type sudo apt install sox into cmd
-duration = .2  # seconds
-freq = 550  # Hz
-os.system('play -nq -t alsa synth {} sine {}'.format(duration, freq))
